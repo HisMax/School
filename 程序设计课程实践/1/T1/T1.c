f6cfab522c48e3b6092b90752cebf63f79e1d3b4
YangMoBy <histonemax@gmail.com>
2022-02-26 01:52:24 +0800

    为T1程序增加了更详细的注释

--- 程序设计课程实践/1/T1/T1.c
+++ 程序设计课程实践/1/T1/T1.c
@@ -3,57 +3,62 @@
 #include <stddef.h>
 struct Num{
     int v;//用来存放具体的值
-    struct Num *next;//下一个节点
+    struct Num *next;//指向下一个节点的指针
 };
-struct Num *head_1,*head_2;
-void Insert(struct Num *p,int v){//插入节点
-    struct Num *new = (struct Num*)malloc(sizeof(struct Num));
-    new->v = v;
-    new->next = p->next;
-    p->next = new;//常规操作，插入一个节点。
+struct Num *head_1,*head_2;//头节点
+
+void Insert(struct Num *p,int v){//p为新节点的上一级节点
+    struct Num *new = (struct Num*)malloc(sizeof(struct Num));//用malloc声明变量，方便之后释放内存
+    new->v = v;//存储数据
+    new->next = p->next;//让新节点的next指向原来p的next
+    p->next = new;//再将p的next指向new节点
 }
-struct Num* Opt(int opt){//opt表示选择哪个链表，对于这道题，完全可以直接写在一起，还可以省一个参数。
+struct Num* Opt(int opt){//opt表示选择哪个链表
     switch (opt){
         case 1:return head_1;
         case 2:return head_2;
-        default:printf("Error List");
+        default:printf("Error List");//假如传入了错误的参数
+                return head_1;//默认返回head_1
     }
 }
 void PrintNum(int opt){
-    struct Num *p = Opt(opt)->next;
+    struct Num *p = Opt(opt)->next;//获取头节点的next
     while(p!=NULL){//遍历列表
-        if(p->next!=NULL)printf("%d->",p->v);
+        if(p->next!=NULL)printf("%d->",p->v);//注意最后一个节点不需要输出 ->
         else printf("%d",p->v);
-        struct Num *temp = p;
+        struct Num *temp = p;//这里需要用一个临时变量存一下p
         p = p->next;
-        free(temp);//内存释放要这样
+        free(temp);
+        /*********************
+         * 你肯定不想这样写
+         * free(p);
+         * p = p->next;
+         * *******************/
     }
-    printf("\n");
+    printf("\n");//这里换行原本是在循环里面的，但是很恶心的发现，正数空表时，答案依然给输出换了一个行
     return;
 }
 void Sort(int v,int opt){//遍历链表，寻找比v大的第一个节点,然后插入
-    struct Num *p = Opt(opt);
+    struct Num *p = Opt(opt);//获取头节点
     while(p->next!=NULL){
-        if(v<(p->next)->v){
-            Insert(p,v);
-            return;
+        if(v<(p->next)->v){//当v小于p下一个节点的v时，在p后面新建节点
+            Insert(p,v);return;
         }
         p = p->next;
     }
-    Insert(p,v);//尾节点特殊待遇
+    Insert(p,v);//循环结束依然没有插进去，那就直接插到末尾。省一个AddEnd()
 }
 
 int main(void){
     int x = 0;
     head_1 = (struct Num*)malloc(sizeof(struct Num));
     head_2 = (struct Num*)malloc(sizeof(struct Num));//申请内存
-    head_1->next = NULL;head_2->next = NULL;
+    head_1->next = NULL;head_2->next = NULL;//注意头节点初始化，next要赋NULL
     while(scanf("%d",&x)!=EOF){
         if(x>0)Sort(x,1);
-        else Sort(x,2);
+        else Sort(x,2);//很简单的一个判断
     }
-    PrintNum(1);PrintNum(2);
-    free(head_1);
-    free(head_2);
+    PrintNum(1);PrintNum(2);//输出答案
+    free(head_1);free(head_2);//最后别忘了释放两个头节点
     return 0;
 }
