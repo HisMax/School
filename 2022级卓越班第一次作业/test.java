79e838484b27c0b63a126a865d33df7d04088ee4
YangMoBy <histonemax@gmail.com>
2022-03-08 15:13:28 +0800

    傻逼T1 按照test里的做法可以拿3分

--- 2022级卓越班第一次作业/test.java
+++ 2022级卓越班第一次作业/test.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
-public class test {
-    public static void main(String[] args){
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+//😇 笑死，要的就是误差
+public class Main{
+    public static void main(String[] args) {
+        float a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextFloat();
+        b = scan.nextFloat();
+        a = (float) Math.round(a/0.45359237 * 10000000) / 10000000;
+        b = (float) Math.round(b/0.0254 * 100000) / 100000;
+        System.out.print(a);
+        System.out.print(" ");
+        System.out.print(b);
     }
 }
